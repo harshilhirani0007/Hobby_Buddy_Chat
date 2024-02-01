814979fca53ee569eaabc37c3d14c458f24bc3a1
harshilhirani0007 <harshilhiranipatel007@gmail.com>
2024-02-01 07:48:58 +0530

    upload

--- app/src/main/java/com/example/hobby_buddy_chat/ProfileSetupActivity.java
+++ app/src/main/java/com/example/hobby_buddy_chat/ProfileSetupActivity.java
@@ -3,6 +3,9 @@ package com.example.hobby_buddy_chat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class ProfileSetupActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class ProfileSetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup);
+
+        Spinner spinner = findViewById(R.id.selectHobby);
+
+        // Fetch the string array from strings.xml
+        String[] hobbiesArray = getResources().getStringArray(R.array.spinner_items);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hobbiesArray);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 }
