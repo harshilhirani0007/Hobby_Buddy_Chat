fee6f6a5b5f6b6292385b3cd8e2698024c11b2aa
harshilhirani0007 <harshilhiranipatel007@gmail.com>
2024-02-11 23:53:25 +0530

    upload

--- app/src/main/java/com/example/hobby_buddy_chat/SignInActivity.java
+++ app/src/main/java/com/example/hobby_buddy_chat/SignInActivity.java
@@ -2,13 +2,28 @@ package com.example.hobby_buddy_chat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class SignInActivity extends AppCompatActivity {
+import com.example.hobby_buddy_chat.databinding.ActivitySignInBinding;
 
+public class SignInActivity extends AppCompatActivity {
+     ActivitySignInBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+
+        binding.tvSignUp.setOnClickListener(v ->
+        {
+            startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+        });
+
+        binding.tvForgotPassword.setOnClickListener(v ->
+        {
+            startActivity(new Intent(SignInActivity.this,ForgotPasswordActivity.class));
+        });
+
     }
 }
