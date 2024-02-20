58454df95cb7827ae214e1e32b3bb0034bb994f9
harshilhirani0007 <harshilhiranipatel007@gmail.com>
2024-02-20 10:07:36 +0530

    upload

--- app/src/main/java/com/example/hobby_buddy_chat/SignInActivity.java
+++ app/src/main/java/com/example/hobby_buddy_chat/SignInActivity.java
@@ -6,16 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hobby_buddy_chat.databinding.ActivitySignInBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
     ActivitySignInBinding binding;
+    FirebaseAuth mAUth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        mAUth = FirebaseAuth.getInstance();
+        
         binding.tvSignUp.setOnClickListener(v ->
         {
             startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
