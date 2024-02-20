62184b54800955c3e87a99c7fd67803536e65976
harshilhirani0007 <harshilhiranipatel007@gmail.com>
2024-02-20 23:04:37 +0530

    upload

--- app/src/main/java/com/example/hobby_buddy_chat/Activities/SignInActivity.java
+++ app/src/main/java/com/example/hobby_buddy_chat/Activities/SignInActivity.java
@@ -1,12 +1,19 @@
 package com.example.hobby_buddy_chat.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.hobby_buddy_chat.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
     ActivitySignInBinding binding;
@@ -29,5 +36,38 @@ public class SignInActivity extends AppCompatActivity {
             startActivity(new Intent(SignInActivity.this, ForgotPasswordActivity.class));
         });
 
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=binding.edtLoginEmail.getText().toString();
+                String password=binding.edtLoginPassword.getText().toString();
+
+                if(password.length() < 8)
+                {
+                    Toast.makeText(SignInActivity.this, "Password contain 8 or more letters", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAUth.signInWithEmailAndPassword(email,password).addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                FirebaseUser user=mAUth.getCurrentUser();
+                                Toast.makeText(SignInActivity.this, "" + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+
+                                Intent i=new Intent(SignInActivity.this, MainActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
+                            else
+                            {
+                                Toast.makeText(SignInActivity.this, "Authentication failed..", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
